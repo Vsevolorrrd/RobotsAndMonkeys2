69c6dceb6e954775f9e328890f8113a5f025fa00
Seva <vsevolod.kolomytsev@student.saedubai.comProfile image>
2025-07-27 12:47:08 +0400

    Buttons and Doors

---
+++ Assets/Scripts/Level/L_Door.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class L_Door : MonoBehaviour
+{
+    [SerializeField] GameObject doorPart;
+    [SerializeField] Animator animator;
+    private Vector3 initialScale;
+    public void OpenDoor(bool state)
+    {
+        GetComponent<Collider2D>().enabled = !state;
+        animator.SetBool("OpenDoor", state);
+
+    }
+    private void HandleReset()
+    {
+        doorPart.transform.localScale = initialScale;
+        StopAllCoroutines();
+    }
+
+    private void Start()
+    {
+        initialScale = transform.localScale;
+        GameManager.Instance.OnGameReset += HandleReset;
+    }
+
+    private void OnDestroy()
+    {
+        if (GameManager.Instance)
+        GameManager.Instance.OnGameReset -= HandleReset;
+    }
+}
--- Assets/Scripts/Level/L_FloorButton.cs
+++ Assets/Scripts/Level/L_FloorButton.cs
@@ -2,5 +2,20 @@ using UnityEngine;
 
 public class L_FloorButton : MonoBehaviour
 {
+    [SerializeField] L_Door[] doors;
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        foreach (var door in doors)
+        {
+            door.OpenDoor(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        foreach (var door in doors)
+        {
+            door.OpenDoor(false);
+        }
+    }
 }
